f646d941ab35f9ce1fbe478fc17a747819372239
elenazahrak <elenazahraa9@gmail.com>
2023-11-11 14:27:55 +0700

    Tugas 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geprek_lensu/menu.dart';
+import 'package:geprek_lensu/screens/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,4 +36,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
